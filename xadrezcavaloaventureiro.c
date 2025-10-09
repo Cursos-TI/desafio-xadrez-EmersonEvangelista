c44bb555a1942a082d6c095c80bd2c2a2ea3566a
EmersonEvangelista <86333588+EmersonEvangelista@users.noreply.github.com>
2025-10-09 00:35:57 +0000

    Peça cavalo TEMA 4 aventureiro.

---
+++ xadrezcavaloaventureiro.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+
+int i, j =1;
+
+    while (j --);
+    {
+        for (i = 0; i < 2; i++){
+
+        printf("Baixo!\n");
+
+        }
+        
+        printf("Direita!\n");
+    } 
+
+}
